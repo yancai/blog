3c8e78fb699e0bc3c76a202e17b22d62188b1e9a
yancai <yancai915@gmail.com>
2017-03-15 15:28:44 +0800

    添加多级标题连接

--- static/js/src/article_page.js
+++ static/js/src/article_page.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".container h1,h2").each(function () {
+    $(".container h1,h2,h3,h4,h5,h6").each(function () {
         var href = $(this).attr("id");
         $(this).prepend('<a class="icon-link" href="#' + href + '" style="visibility: hidden"></a>');
     }).hover(function () {
@@ -8,5 +8,3 @@ $(document).ready(function () {
         $(this).children("a").css("visibility", "hidden");
     });
 });
-
-
